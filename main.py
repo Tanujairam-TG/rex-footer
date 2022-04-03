5a719ed8d890cdc8ef0605f9dd369676077b2d18
Tanujairam <88611197+Tanujairam123@users.noreply.github.com>
2022-04-03 20:42:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def _start(bot: Client, m: Message):
     await AddUserToDatabase(bot, m)
     try:
         await m.reply_text(
-            Config.START_TEXT.format(name=m.from_user.first_name),
+            Config.START_TEXT.format(name=m.from_user.mention),
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("Updates", url="https://t.me/REX_BOTZ"), InlineKeyboardButton("Support group", url="https://t.me/REX_Bots_Support")]
